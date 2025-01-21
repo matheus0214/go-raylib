fe436ebc34dd71020725fdbba8e4880ee499bcbc
Matheus G. Dias <matheusgiga123@gmail.com>
2025-01-21 15:31:55 -0300

    create main window

---
+++ configs/configs.go
@@ -0,0 +1,8 @@
+package configs
+
+const (
+	FPS           = 60
+	WINDOW_WIDTH  = 1200
+	WINDOW_HEIGHT = 800
+	GAME_NAME     = "Explorer"
+)
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+	"github.com/matheus0214/go-raylib/configs"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	rl.InitWindow(configs.WINDOW_WIDTH, configs.WINDOW_HEIGHT, configs.GAME_NAME)
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(configs.FPS)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.White)
+		rl.DrawText("Congrats", 190, 100, 20, rl.Black)
+
+		rl.EndDrawing()
+	}
 }
